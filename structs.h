0192124c9963ce0757923e29c2c9ceaa7e36ea11
simonp <simon.plasger@web.de>
2013-04-01 23:47:13 +0200

    Started adding useful comments

--- structs.h
+++ structs.h
@@ -10,5 +10,5 @@ struct elektrol_config /* Structure where the configuration will be stored */
     char *passwd;
     char *delay;
     char *outputdir;
-    struct tm time;
+    struct tm time; // Structure where time can be stored.
 };
